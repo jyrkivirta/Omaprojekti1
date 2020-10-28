4ee46467b2da503cc25545479096ae6f7b956e31
jyrkivirta <jyrki.virta@gmail.com>
2020-10-28 15:51:54 +0200

    listahomma melkein toiii

--- Listat/listat/src/App.js
+++ Listat/listat/src/App.js
@@ -2,23 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 
-const [lista, setLista] = useState(['John', 'Paul', 'Ringo','George']);
-const [listaKakkonen, setListaKakkonen] = useState(['Juhani','Tuomas','Aapo','Simeoni','Timo','Lauri','Eero']);
 
-const siirraVasemmalle = () => {};
-const siirraOikealle = () => {};
-
-const indexLista = ()
 
 function App() {
+
+  const [lista, setLista] = useState(['John', 'Paul', 'Ringo','George']);  //vasen lista
+  const [listaKakkonen, setListaKakkonen] = useState(['Juhani','Tuomas','Aapo','Simeoni','Timo','Lauri','Eero']); //oikea lista
+  const [uusiLista, setUusiLista] = useState([])  //lista johon kirjataan mitä on klikattu
+  
+  const indexLista = (item, index) => {           //kun klikataan, klikattu nimi tulis tässä listaan
+    let listaTila = uusiLista.concat(item)
+    setLista(listaTila);
+  };
+
+  const siirraOikealle = () => {            // kun nuoli-nappia painetaan, tämä heittää uusiListan oikean listan perään
+    let siirtoLista = listaKakkonen.concat(uusiLista);  // ja tähän pitäs sit lisätä nimen poistaminen vasemmasta listasta
+    setUusiLista(siirtoLista);
+  };
+  const siirraVasemmalle = () => {};
+  
+
   return (
     <div> {
-      lista.map((item, index) => <div onClick={}>{index + '. ' + item}</div>)
+      lista.map((item, index) => <p onClick={indexLista(item, index)}>{index + '. ' + item}</p>)
       } 
         <button onClick={siirraVasemmalle()}>{'<-'}</button>
         <button onClick={siirraOikealle()}>{'->'}</button>
         {
-      listaKakkonen.map((item, index) => <div>{index + '. ' + item}</div>)
+      listaKakkonen.map((item, index) => <p onClick={indexLista(item, index)}>{index + '. ' + item}</p>)
       }
     </div>
   );
