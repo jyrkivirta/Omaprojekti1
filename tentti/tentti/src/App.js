207d405da2af5ff2e730bf7ab22973c3874575be
jyrkivirta <jyrki.virta@gmail.com>
2020-11-13 15:00:31 +0200

    Reducer toimii kuin sveitsiläinen kello

--- tentti/tentti/src/App.js
+++ tentti/tentti/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles/'
+//import { makeStyles } from '@material-ui/core/styles/'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 //import Typography from '@material-ui/core/Typography'
@@ -17,82 +17,132 @@ import AddCircle from '@material-ui/icons/AddCircle';
 import TextField from '@material-ui/core/TextField';
 import uuid from 'react-uuid';
 //import School from '@material-ui/School';
+import { useReducer } from 'react';
+
+const initialData = [
+  {uid:uuid(), tentti: "tentti ykkönen", kysymykset:[
+        {
+          uid:uuid(), kysymys: "Kuka seuraavista kirjailijoista ei kuulu tämän vuoden Finlandia-palkintoehdokkaisiin?", vaihtoehdot: 
+            [{uid:uuid(),vastaus: "Tommi Kinnunen", valittu:false, oikein:false},
+            {uid:uuid(),vastaus: "Johannes Ekholm", valittu:false, oikein:true},
+            {uid:uuid(),vastaus: "Ann-Luise Bertell", valittu:false, oikein:false},
+            {uid:uuid(),vastaus: "Anni Kytömäki", valittu:false, oikein:false}]
+        },
+        {
+          uid:uuid(), kysymys: "Ketkä kolme kirjailijaa ovat voittaneet viimeiset kolme Finlandia-palkintoa?", vaihtoehdot: 
+            [{uid:uuid(),vastaus: "Juha Hurme, Laura Lindstedt, Jukka Viikilä", valittu:false, oikein:false}, 
+            {uid:uuid(),vastaus: "Olli Jalonen, Jukka Viikilä, Laura Lindstedt", valittu:false, oikein:false}, 
+            {uid:uuid(),vastaus: "Pajtim Statovci, Olli Jalonen, Juha Hurme", valittu:false, oikein:true}, 
+            {uid:uuid(),vastaus: "Laura Lindstedt, Olli Jalonen, Pajtim Statovci", valittu:false, oikein:false}]
+        },
+        {
+          uid:uuid(), kysymys: "Kuka seuraavista kirjailijoista on saanut kaksi Finlandia-palkintoa?", vaihtoehdot: 
+            [{uid:uuid(),vastaus: "Johanna Sinisalo", valittu:false, oikein:false}, 
+            {uid:uuid(),vastaus: "Kari Hotakainen", valittu: false, oikein:false}, 
+            {uid:uuid(),vastaus: "Rosa Liksom", valittu:false, oikein:false}, 
+            {uid:uuid(),vastaus: "Bo Carpelan", valittu:false, oikein:true}]
+        },
+        {
+          uid:uuid(), kysymys: "Kenen runoilijan elämästä kertoo vuoden 2000 Finlandia-palkintokirja Helena Sinervon Runoilijan talossa?", vaihtoehdot: 
+            [{uid:uuid(),vastaus: "Yrjö Kaijärvi", valittu:false, oikein:false}, 
+            {uid:uuid(),vastaus: "Sirkka Turkka", valittu: false, oikein:false}, 
+            {uid:uuid(),vastaus: "Eeva-Liisa Manner", valittu:false, oikein:true}, 
+            {uid:uuid(),vastaus: "Aila Meriluoto", valittu:false, oikein:false}]
+        }
+      ]
+      },
+  {uid:uuid(), tentti: "tentti kakkonen", kysymykset:[
+        {
+          uid:uuid(), kysymys: "Uusi kysymys ykkönen", vaihtoehdot: 
+            [{uid:uuid(),vastaus: "vastaus I", valittu:false, oikein:false},
+            {uid:uuid(),vastaus: "vastaus II", valittu:false, oikein:true}]
+        },
+        {
+          uid:uuid(), kysymys: "Uusi kysymys kakkonen", vaihtoehdot: 
+            [{uid:uuid(),vastaus: "vastaus 1", valittu:false, oikein:false}, 
+            {uid:uuid(),vastaus: "vastaus 2", valittu:false, oikein:false}]
+        },
+        {
+          uid:uuid(), kysymys: "Uusi kysymys kolmonen", vaihtoehdot: 
+            [{uid:uuid(),vastaus: "vastaus a", valittu:false, oikein:false}, 
+            {uid:uuid(),vastaus: "vastaus b", valittu: false, oikein:false} 
+            ]
+        },
+        {
+          uid:uuid(), kysymys: "Uusi kysymys nelonen", vaihtoehdot: 
+            [{uid:uuid(),vastaus: "vastaus x", valittu:false, oikein:false}, 
+            {uid:uuid(),vastaus: "vastaus y", valittu: false, oikein:false}]
+        }
+      ]
+      }
+    ]
 
 
+function reducer(state, action) {
+  
+  let uusData = JSON.parse(JSON.stringify(state))
+ 
+  switch (action.type) {
+    case "INIT_DATA":
+      return action.data;
+
+    case "TEMP_DATA":
+      return JSON.parse(action.data);
+
+    case "JOTAIN_TAPAHTUU":
+      uusData[action.data.valittuTentti].kysymykset[action.data.kysymysIndex].vaihtoehdot[action.data.vaihtoehtoIndex].valittu=action.data.event.target.checked;
+      return uusData;
+
+    case "INPUT_MUUTETTU":
+      uusData[action.data.valittuTentti].kysymykset[action.data.kysymysIndex].vaihtoehdot[action.data.vaihtoehtoIndex].vastaus=action.data.event.target.value;
+      return uusData;
+    
+    case "KYSYMYS_MUUTETTU":
+      uusData[action.data.valittuTentti].kysymykset[action.data.kysymysIndex].kysymys=action.data.event.target.value;
+      return uusData;
+      
+    case "POISTA_VAIHTOEHTO":
+      uusData[action.data.valittuTentti].kysymykset[action.data.kysymysIndex].vaihtoehdot.splice(action.data.vaihtoehtoIndex, 1);
+      return uusData;
+
+    case "LISÄÄ_VAIHTOEHTO":
+      uusData[action.data.valittuTentti].kysymykset[action.data.kysymysIndex].vaihtoehdot.push({uid:uuid(), vastaus: "", valittu: false, oikein: false});
+      return uusData;
+      
+    case "LISÄÄ_KYSYMYS":
+      uusData[action.data.valittuTentti].kysymykset.push({uid:uuid(), kysymys:"", vaihtoehdot:[{uid:uuid(),vastaus: "", valittu:false, oikein:false}]});
+      return uusData;
+      
+    case "POISTA_KYSYMYS":
+      uusData[action.data.valittuTentti].kysymykset.splice(action.data.kysymysIndex);
+      return uusData;
+
+    case "LISÄÄ_TENTTI":
+      uusData.push({uid:uuid(),tentti:"Uusi tentti", kysymykset:[{uid:uuid(), kysymys:"", vaihtoehdot:[{uid:uuid(),vastaus: "", valittu:false, oikein:false}] }] });
+      return uusData;
+
+    case "POISTA_TENTTI":
+      uusData.splice(-1,1);
+      return uusData;
+
+    default:
+      throw new Error();
+  }
+}
 
 function App () {
  
-  const initialData = [
-    {uid:uuid(), tentti: "tentti ykkönen", kysymykset:[
-          {
-            uid:uuid(), kysymys: "Kuka seuraavista kirjailijoista ei kuulu tämän vuoden Finlandia-palkintoehdokkaisiin?", vaihtoehdot: 
-              [{uid:uuid(),vastaus: "Tommi Kinnunen", valittu:false, oikein:false},
-              {uid:uuid(),vastaus: "Johannes Ekholm", valittu:false, oikein:true},
-              {uid:uuid(),vastaus: "Ann-Luise Bertell", valittu:false, oikein:false},
-              {uid:uuid(),vastaus: "Anni Kytömäki", valittu:false, oikein:false}]
-          },
-          {
-            uid:uuid(), kysymys: "Ketkä kolme kirjailijaa ovat voittaneet viimeiset kolme Finlandia-palkintoa?", vaihtoehdot: 
-              [{uid:uuid(),vastaus: "Juha Hurme, Laura Lindstedt, Jukka Viikilä", valittu:false, oikein:false}, 
-              {uid:uuid(),vastaus: "Olli Jalonen, Jukka Viikilä, Laura Lindstedt", valittu:false, oikein:false}, 
-              {uid:uuid(),vastaus: "Pajtim Statovci, Olli Jalonen, Juha Hurme", valittu:false, oikein:true}, 
-              {uid:uuid(),vastaus: "Laura Lindstedt, Olli Jalonen, Pajtim Statovci", valittu:false, oikein:false}]
-          },
-          {
-            uid:uuid(), kysymys: "Kuka seuraavista kirjailijoista on saanut kaksi Finlandia-palkintoa?", vaihtoehdot: 
-              [{uid:uuid(),vastaus: "Johanna Sinisalo", valittu:false, oikein:false}, 
-              {uid:uuid(),vastaus: "Kari Hotakainen", valittu: false, oikein:false}, 
-              {uid:uuid(),vastaus: "Rosa Liksom", valittu:false, oikein:false}, 
-              {uid:uuid(),vastaus: "Bo Carpelan", valittu:false, oikein:true}]
-          },
-          {
-            uid:uuid(), kysymys: "Kenen runoilijan elämästä kertoo vuoden 2000 Finlandia-palkintokirja Helena Sinervon Runoilijan talossa?", vaihtoehdot: 
-              [{uid:uuid(),vastaus: "Yrjö Kaijärvi", valittu:false, oikein:false}, 
-              {uid:uuid(),vastaus: "Sirkka Turkka", valittu: false, oikein:false}, 
-              {uid:uuid(),vastaus: "Eeva-Liisa Manner", valittu:false, oikein:true}, 
-              {uid:uuid(),vastaus: "Aila Meriluoto", valittu:false, oikein:false}]
-          }
-        ]
-        },
-    {uid:uuid(), tentti: "tentti kakkonen", kysymykset:[
-          {
-            uid:uuid(), kysymys: "Uusi kysymys ykkönen", vaihtoehdot: 
-              [{uid:uuid(),vastaus: "vastaus I", valittu:false, oikein:false},
-              {uid:uuid(),vastaus: "vastaus II", valittu:false, oikein:true}]
-          },
-          {
-            uid:uuid(), kysymys: "Uusi kysymys kakkonen", vaihtoehdot: 
-              [{uid:uuid(),vastaus: "vastaus 1", valittu:false, oikein:false}, 
-              {uid:uuid(),vastaus: "vastaus 2", valittu:false, oikein:false}]
-          },
-          {
-            uid:uuid(), kysymys: "Uusi kysymys kolmonen", vaihtoehdot: 
-              [{uid:uuid(),vastaus: "vastaus a", valittu:false, oikein:false}, 
-              {uid:uuid(),vastaus: "vastaus b", valittu: false, oikein:false} 
-              ]
-          },
-          {
-            uid:uuid(), kysymys: "Uusi kysymys nelonen", vaihtoehdot: 
-              [{uid:uuid(),vastaus: "vastaus x", valittu:false, oikein:false}, 
-              {uid:uuid(),vastaus: "vastaus y", valittu: false, oikein:false}]
-          }
-        ]
-        }
-      ]
+  const [state, dispatch] = useReducer(reducer, initialData);
 
-  const [data, setData] = useState(initialData)
+  // const [data, setData] = useState(initialData)
  
   const [painettu, setPainettu] = useState(false)
   
   const [valittuTentti, setValittuTentti] = useState(0)
 
-  const useStyles = makeStyles(() => ({
-    flexTyyli: {
-      flex: 1,
-    }
-  }));
-  
-  const classes = useStyles();
+
+// USE EFFECTIT
+
 
   useEffect(() => {
     let jemma = window.localStorage;
@@ -101,92 +151,41 @@ function App () {
       jemma.setItem("data", JSON.stringify(initialData))
       tempData = initialData
     }
-    setData(JSON.parse(tempData));
+    dispatch({ type: "TEMP_DATA", data: tempData })
+    //setData(JSON.parse(tempData));
   },
     [])
+    
   useEffect(() => {
-    window.localStorage.setItem("data", JSON.stringify(data))
+    window.localStorage.setItem("data", JSON.stringify(state))
   },
-    [data])
+    [state])
 
+
+//FUNKTIOT
+
+
+const aktiivinenTentti = (tenttiIndex) => {
+  setValittuTentti(tenttiIndex);
+}
 
 const tyhjennaLomake = () => {
-  setData(initialData);
+  dispatch({type:"INIT_DATA", data: initialData});
+  // setData(initialData);
+  //nämä funktiot olisi ehkä voinut laittaa myös reduceriin, mutta en ollut varma voiko reducerin sisällä tehdä lenkkiä
 }
  
 const tyhjennaLocal = () => {
   localStorage.clear()
-  setData(initialData);
-}
-
-
-const jotainTapahtuu = (kysymysIndex, vaihtoehtoIndex, event, valittuTentti) => {
-  
-
-  let uusData = JSON.parse(JSON.stringify(data))
-  uusData[valittuTentti].kysymykset[kysymysIndex].vaihtoehdot[vaihtoehtoIndex].valittu=event.target.checked
-  setData(uusData)
-  //console.log(event)
-
+  dispatch({type:"INIT_DATA", data: initialData});
+  // setData(initialData);
 }
 
 const vastauksetPainettu = () => {
   let nappi = painettu
   nappi = !nappi
   setPainettu(nappi)
-} 
-
-const inputMuutettu = (event, kysymysIndex, vaihtoehtoIndex, valittuTentti) => {
-  let uusData = JSON.parse(JSON.stringify(data))
-  uusData[valittuTentti].kysymykset[kysymysIndex].vaihtoehdot[vaihtoehtoIndex].vastaus=event.target.value
-  setData(uusData);
 }
-
-const kysymysMuutettu = (kysymysIndex, event, valittuTentti) => {
-  let uusData = JSON.parse(JSON.stringify(data))
-  uusData[valittuTentti].kysymykset[kysymysIndex].kysymys=event.target.value
-  setData(uusData);
-}
-
-const poistaVaihtoehto = (kysymysIndex, vaihtoehtoIndex, valittuTentti) => {
-  let uusData = JSON.parse(JSON.stringify(data))
-  uusData[valittuTentti].kysymykset[kysymysIndex].vaihtoehdot.splice(vaihtoehtoIndex, 1)
-  setData(uusData);
-}
-
-const lisaaVaihtoehto = (kysymysIndex, valittuTentti) => {
-  let uusData = JSON.parse(JSON.stringify(data))
-  uusData[valittuTentti].kysymykset[kysymysIndex].vaihtoehdot.push({uid:uuid(),vastaus: "", valittu:false, oikein:false})
-  setData(uusData)
-}
-const lisaaKysymys = (valittuTentti) => {
-  let uusData = JSON.parse(JSON.stringify(data))
-  uusData[valittuTentti].kysymykset.push({uid:uuid(), kysymys:"", vaihtoehdot:[{uid:uuid(),vastaus: "", valittu:false, oikein:false}]})
-  setData(uusData)
-}
-
-const poistaKysymys = (kysymysIndex, valittuTentti) => {
-  let uusData = JSON.parse(JSON.stringify(data))
-  uusData[valittuTentti].kysymykset.splice(kysymysIndex, 1)
-  setData(uusData);
-}
-
-const lisaaTentti = (dataIndex) => {
-  let uusData = JSON.parse(JSON.stringify(data))
-  uusData.push({uid:uuid(),tentti:"Uusi tentti", kysymykset:[{uid:uuid(), kysymys:"", vaihtoehdot:[{uid:uuid(),vastaus: "", valittu:false, oikein:false}]}]})
-  setData(uusData)
-}
-
-const poistaTentti = () => {
-  let uusData = JSON.parse(JSON.stringify(data))
-  uusData.splice(-1, 1)
-  setData(uusData);
-}
-
-const aktiivinenTentti = (tenttiIndex) => {
-  setValittuTentti(tenttiIndex);
-}
-
 
 const naytaVaihtoehdot = (vaihtoehdot, kysymysIndex, valittuTentti) => {
   return vaihtoehdot.map((vaihtoehto, vaihtoehtoIndex) => <div> 
@@ -194,7 +193,7 @@ const naytaVaihtoehdot = (vaihtoehdot, kysymysIndex, valittuTentti) => {
        <Checkbox
           id={vaihtoehto.vastaus} 
           name={vaihtoehto.vastaus} 
-          onChange={(event) => jotainTapahtuu(kysymysIndex, vaihtoehtoIndex, event, valittuTentti)}
+          onChange={(event) => dispatch({type:"JOTAIN_TAPAHTUU", data: {kysymysIndex: kysymysIndex, vaihtoehtoIndex: vaihtoehtoIndex, event: event, valittuTentti: valittuTentti} }) }
           checked={vaihtoehto.valittu}
         ></Checkbox>
        {painettu && 
@@ -211,13 +210,13 @@ const naytaVaihtoehdot = (vaihtoehdot, kysymysIndex, valittuTentti) => {
               variant="outlined"
               type="text"
               style = {{width: 450}}
-              onChange={(event) => inputMuutettu(event, kysymysIndex, vaihtoehtoIndex, valittuTentti)}
+              onChange={(event) => dispatch({type:"INPUT_MUUTETTU", data: {event: event, kysymysIndex: kysymysIndex, vaihtoehtoIndex: vaihtoehtoIndex, valittuTentti: valittuTentti} }) }
               value={vaihtoehto.vastaus}>
           </TextField>
           {/*tästä kohtaa jos ottaa inputin pois ja laittaa tilalle {vaihtoehto.vastaus}, niin tulee oppilaan näkymä*/}
         </label>
         <IconButton
-          onClick={() => poistaVaihtoehto(kysymysIndex, vaihtoehtoIndex, valittuTentti)}
+          onClick={() => dispatch({type:"POISTA_VAIHTOEHTO", data:{kysymysIndex: kysymysIndex, vaihtoehtoIndex: vaihtoehtoIndex, valittuTentti: valittuTentti} }) }
         ><Delete />
         </IconButton>
   </CardContent>      
@@ -233,14 +232,15 @@ const naytaVaihtoehdot = (vaihtoehdot, kysymysIndex, valittuTentti) => {
     </AppBar>
     <Card><CardContent>
     <div>
-      {data.map((tentti, index) => 
-        <Button onClick={() => aktiivinenTentti(index)}> {tentti.tentti} </Button>
+      {state.map((tentti, index) => 
+        <Button key={index.uid} onClick={() => aktiivinenTentti(index)}> 
+        {tentti.tentti} </Button>
       )}
-      <IconButton onClick={() => lisaaTentti()}
+      <IconButton onClick={() => dispatch({type: "LISÄÄ_TENTTI", data:{}  }) } 
         ><AddCircle />
       </IconButton>
     </div>
-    {data[valittuTentti].kysymykset.map(( {kysymys, vaihtoehdot}, index) => {
+    {state[valittuTentti].kysymykset.map(( {kysymys, vaihtoehdot}, index) => {
       return (
        <div>
         <CardContent key={index.uid}>
@@ -249,19 +249,19 @@ const naytaVaihtoehdot = (vaihtoehdot, kysymysIndex, valittuTentti) => {
               variant="outlined"
               type="text"
               style = {{width: 800}}
-              onChange={(event) => kysymysMuutettu(index, event, valittuTentti)}
+              onChange={(event) => dispatch({type:"KYSYMYS_MUUTETTU", data: {kysymysIndex:index, event: event, valittuTentti: valittuTentti} }) }
               value={kysymys}>
           </TextField>
           {/*tästä kohtaa jos ottaa textfieldin pois ja laittaa tilalle {kysymys}, niin tulee oppilaan näkymä*/}
         
           <IconButton
-            onClick={() => poistaKysymys(index, valittuTentti)}
+            onClick={() => dispatch({type:"POISTA_KYSYMYS", data:{kysymysIndex: index, valittuTentti:valittuTentti} }) }
             ><Delete />
           </IconButton>
         
         {naytaVaihtoehdot(vaihtoehdot, index, valittuTentti)}
           <IconButton
-            onClick={() => lisaaVaihtoehto(index, valittuTentti)}
+            onClick={() => dispatch({type:"LISÄÄ_VAIHTOEHTO", data: {kysymysIndex: index, valittuTentti: valittuTentti} }) }
           ><AddCircle />
           </IconButton>
         </CardContent> 
@@ -270,7 +270,7 @@ const naytaVaihtoehdot = (vaihtoehdot, kysymysIndex, valittuTentti) => {
     }
     )}</CardContent></Card>
       <IconButton
-          onClick={() => lisaaKysymys(valittuTentti)}
+          onClick={() => dispatch({type:"LISÄÄ_KYSYMYS", data:{valittuTentti: valittuTentti} }) }
           ><AddCircle />
       </IconButton>
       {/* <Button
